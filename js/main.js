3f113b6161d05fad40aad11607a0af7eaf43979f
darinadgc <69759782+darinadgc@users.noreply.github.com>
2020-09-11 18:34:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger, nav,  .wrapper').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('#btn').click(function(event) {
+		$('.places,.sites').toggleClass('active');
+		
+	});
+});
 
+//$(function () {
+	
+//     $('.main-menu li').click(function (){
+//         $(this).toggleClass('active');
+//     });
+	
+// 	$(window).scroll(function() {
+// 	    $('.works-wrap__item').each(function(){
+// 	        var imagePos = $(this).offset().top;
+
+// 	        var topOfWindow = $(window).scrollTop();
+// 	        if (imagePos < topOfWindow+650) {
+// 	            $(this).addClass("flipInX");
+// 	        }
+// 	    });
+// 	});
+	
+// 	$(window).scroll(function() {
+// 	    $('.animated').each(function(){
+// 	        var imagePos = $(this).offset().top;
+
+// 	        var topOfWindow = $(window).scrollTop();
+// 	        if (imagePos < topOfWindow+650) {
+// 	            $(this).css("opacity", "1");
+// 	        }
+// 	    });
+// 	});
+// })
